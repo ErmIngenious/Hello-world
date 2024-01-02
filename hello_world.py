3733332b56b2343d1e3fbc4ca4bdeb54e3746536
Emma-Louise Moses <148122558+ErmIngenious@users.noreply.github.com>
2024-01-02 03:54:01 +0000

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,3 @@
-'''
-ask users name and store this input in 
-a variable and print name.
-ask users age store this input in a variable and print age
-print the string Hello World on a new line.
-'''
-
 #greets and asks for user name then stores this in a variable. 
 name = input("Welcome. Please enter your name: " )
 
